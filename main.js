42f817ec8a21dbb9158466e55d0bfa35522ae52b
OdinM13 <markodinius@gmail.com>
2026-04-29 21:21:59 +0200

    WIP: Build basic structure of classes and functions

---
+++ main.js
@@ -0,0 +1,83 @@
+class Tree {
+  constructor(array) {
+    this.root = buildTree(array);
+  }
+
+  buildTree(array) {
+  // takes an array of numbers and turns it into a balanced binary tree
+  // return level-0 root node
+  }
+
+  includes(value) {
+  // accepts a value and returns true if the given value is in the tree. Else false
+  }
+
+  insert(value) {
+  // accepts a value and inserts a new node with that value into the tree
+  // if the function is called with a value that already exists in the tree, the function should do nothing
+  }
+
+  deleteItem(value) {
+  // accepts a value and removes it from the tree
+  // if the given value doesn't exist in the tree, the function should do nothing
+  }
+
+  levelOrderForEach(callback) {
+  // accepts a callback function as its parameter. levelOrderForEach() should traverse the tree in breadth-first level order and call the callback on each value
+  // as it traverses, passing each value (not the nodes) as an argument
+  // If no callback function is provided, throw an Error reporting that a callback is required
+  }
+
+  inOrderForEach(callback) {
+
+  } 
+
+  preOrderForEach(callback) {
+
+  }
+
+  postOrderForEach(callback) {
+
+  }
+
+  height(value) {
+  // returns the height of the node containing the given value
+  // Height is defined as the number of edges in the longest path from that node to a leaf node
+  // If the value is not found in the tree, the function should return undefined.
+  }
+
+  depth(value) {
+  // returns the depth of the node containing the given value
+  // Depth is defined as the number of edges in the path from that node to the root node
+  // If the value is not found in the tree, the function should return undefined.
+  }
+
+  isBalanced() {
+  // checks if the tree is balanced
+  // A binary tree is considered balanced if, for every node in the tree, 
+  // the height difference between its left and right subtrees is no more than 1, and both the left and right subtrees are also balanced.
+  }
+
+  rebalance() {
+  // rebalances an unbalanced tree
+  // You’ll want to use a traversal method to provide a new array to the buildTree() function.
+  }
+}
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null || node === undefined) {
+    return;
+  }
+
+  prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+}
